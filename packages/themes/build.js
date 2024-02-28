a55fbc9026484213362eb30b064472e5fc79e08b
lsj0202 <w1004123@gmail.com>
2024-02-28 17:53:21 +0900

    feat: add esbuild 환경설정

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,40 +1,6 @@
-import esbuild from "esbuild";
+import run from "@lsj/esbuild-config";
 import pkg from "./package.json" assert { type: "json" };
 
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-
-const watch = process.argv.includes("--watch");
-
-const external = Object.keys({
-  ...pkg.dependencies,
-  ...pkg.peerDependencies,
-});
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  target: "es2019",
-  watch,
-  external,
-};
-
-Promise.all([
-  esbuild.build({
-    ...baseConfig,
-    format: "esm",
-  }),
-  esbuild.build({
-    ...baseConfig,
-    format: "cjs",
-    outExtension: {
-      ".js": ".cjs",
-    },
-  }),
-]).catch(() => {
-  console.error("Build failed");
-  precess.exit(1);
+run({
+  pkg,
 });
